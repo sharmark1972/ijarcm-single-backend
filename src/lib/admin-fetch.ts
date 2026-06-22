165217205d28f3e7a8b069d8dd9135d6fba1e3ed
sharmark1972 <sharmark1972@gmail.com>
2026-06-22 19:27:25 +0530

    header x-site-slug add

---
+++ src/lib/admin-fetch.ts
@@ -0,0 +1,18 @@
+'use client';
+
+export async function adminFetch(
+  input: string,
+  init: RequestInit = {}
+): Promise<Response> {
+  const extraHeaders: Record<string, string> = {
+    'x-site-slug': 'ijarcm'
+  };
+
+  return fetch(input, {
+    ...init,
+    headers: {
+      ...(init.headers ?? {}),
+      ...extraHeaders,
+    },
+  });
+}
